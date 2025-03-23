33bc12c18e61ee68b2a96d1b299b49f76e2e55d4
Souptim <souptim2004@gmail.com>
2025-03-23 20:38:29 +0530

    Home page setup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import { ReactNode } from 'react'
 
 const RootLayout = ({children}: {children: ReactNode}) => {
   return (
-    <div>{children}</div>
+    <div className='root-layout'>
+      <nav>
+        <Link href="/" className='flex items-center gap-2'>
+          <Image src="/logo.png" alt="Logo" width={150} height={120}/>
+        </Link>
+      </nav>
+      {children}
+    </div>
   )
 }
 
